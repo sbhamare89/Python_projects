408c24d9cc6fabe8ee8400e0f28a41d6c680090f
Sagar  Bhamare <80466696+sbhamare89@users.noreply.github.com>
2023-05-06 09:21:40 +0530

    Update restart_pod_concurrency.py

--- kube_oc/restart_pod_concurrency.py
+++ kube_oc/restart_pod_concurrency.py
@@ -1,28 +1,31 @@
 import os
 import time
 import datetime
-from tqdm import tqdm
 import json
-import threading
+import argparse
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
+
+# Define argument parser to accept optional arguments for concurrency and batch size
+parser = argparse.ArgumentParser(description='Delete and restart pods in a given namespace.')
+parser.add_argument('-c', '--concurrency', type=int, default=1, help='Number of threads to use for concurrent execution.')
+parser.add_argument('-b', '--batch-size', type=int, default=5, help='Batch size for deleting pods.')
+args = parser.parse_args()
 
 # Print script information for the user
 print("This script will delete all the healthy pods in a given namespace in batches and then wait for them to restart.")
 print("You can set the batch size and sleep interval between batches to customize the script behavior.")
+print("You can also set the number of threads to use for concurrent execution.")
 
 # Get the namespace where you want to restart pods
 NAMESPACE = input("Enter the namespace where you want to restart pods (default): ").strip() or "default"
 
 # Get the batch size for deleting pods
-batch_size = input("Enter the batch size for deleting pods (5): ").strip() or "5"
-BATCH_SIZE = int(batch_size)
+BATCH_SIZE = args.batch_size
 
 # Get the sleep interval between batches
 sleep_interval = input("Enter the sleep interval between batches in seconds (5): ").strip() or "5"
 SLEEP_INTERVAL = int(sleep_interval)
-
-# Get the number of concurrent threads
-num_threads = input("Enter the number of concurrent threads (1): ").strip() or "1"
-NUM_THREADS = int(num_threads)
 
 # Generate a unique log file name based on the current date and time
 now = datetime.datetime.now()
@@ -42,37 +45,30 @@ num_pods = len(pod_list)
 # Calculate the number of batches
 num_batches = (num_pods + BATCH_SIZE - 1) // BATCH_SIZE
 
-# Delete all the pods in batches
-num_restarted_pods = 0
-lock = threading.Lock()
-
+# Define function to delete pods in a batch
 def delete_pods(pod_list_to_delete):
-    global num_restarted_pods
-    # Delete the pods
-    cmd = f"kubectl delete pods -n {NAMESPACE} {' '.join(pod_list_to_delete)}"
-    print(f"Running command : {cmd}")
-    os.system(cmd)
-
-    # Wait for the pods to be terminated
-    time.sleep(SLEEP_INTERVAL)
-
-    # Calculate the number of pods that have been restarted so far
-    with lock:
-        num_restarted_pods += len(pod_list_to_delete)
-
     # Log the pods being deleted
     with open(LOG_FILE, "a") as log_file:
         log_file.write(f"{datetime.datetime.now()}: Deleting pods: {pod_list_to_delete}\n")
 
-    # Print the status message with the progress bar, pod names, and pod counts
-    tqdm.write(f"Restarted {num_restarted_pods}/{num_pods} pods: {pod_list_to_delete}.")
+    # Delete the pods
+    cmd = f"kubectl delete pods -n {NAMESPACE} {' '.join(pod_list_to_delete)}"
+    os.system(cmd)
 
-def delete_pods_threaded(pod_list_to_delete):
-    threads = []
-    for i in range(NUM_THREADS):
-        start_index = i * (len(pod_list_to_delete) // NUM_THREADS)
-        end_index = (i + 1) * (len(pod_list_to_delete) // NUM_THREADS)
-        if i == NUM_THREADS - 1:
-            end_index = len(pod_list_to_delete)
-        pod_list_to_delete_thread = pod_list_to_delete[start_index:end_index]
-        t = threading.Thread(target=delete
+# Use a thread pool executor to perform concurrent execution
+with ThreadPoolExecutor(max_workers=args.concurrency) as executor:
+    # Delete all the pods in batches
+    num_restarted_pods = 0
+    for i in tqdm(range(num_batches), desc="Deleting pods", total=num_batches):
+        # Get the list of pods to delete in this batch
+        start_index = i * BATCH_SIZE
+        end_index = (i + 1) * BATCH_SIZE
+        pod_list_to_delete = pod_list[start_index:end_index]
+
+        # Execute the delete_pods function in a separate thread
+        executor.submit(delete_pods, pod_list_to_delete)
+
+        # Wait for the pods to be terminated
+        time.sleep(SLEEP_INTERVAL)
+
+        # Calculate the number of pods that have been restarted so far
